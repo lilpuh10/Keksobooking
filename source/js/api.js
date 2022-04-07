e0990b96a4b4fb2f481cae9d2066b2bc9e587de9
Asan Azimkulov <azimkulov40@gmail.com>
2022-04-07 13:48:35 +0600

    np

--- source/js/api.js
+++ source/js/api.js
@@ -1,30 +1,30 @@
 const getData = (url, onSuccess, onFail) => {
-  fetch(url)
-    .then ((response => {
-      if (response.ok) {
-        return response.json();
-      }
-      onFail(`Не удалось загрузить данные об объектах: ${response.text}`);
-    }))
-    .then(onSuccess)
-    .catch(onFail);
+    fetch(url)
+        .then((response => {
+            if (response.ok) {
+                return response.json();
+            }
+            onFail(`Не удалось загрузить данные об объектах: ${response.text}`);
+        }))
+        .then(onSuccess)
+        .catch(onFail);
 };
 
 const sendData = (url, onSuccess, onFail, body) => {
-  fetch(
-    url,
-    {
-      method: 'POST',
-      body,
-    },
-  )
-    .then((response) => {
-      if (response.ok) {
-        return onSuccess();
-      }
-      onFail();
-    })
-    .catch(onFail);
+    fetch(
+        url,
+        {
+            method: 'POST',
+            body,
+        },
+    )
+        .then((response) => {
+            if (response.ok) {
+                return onSuccess();
+            }
+            onFail();
+        })
+        .catch(onFail);
 };
 
 
